10166c779125209af7ff2eb4f8aec97d80a3d4c0
mar173 <kiker173@wp.pl>
2017-01-28 23:01:36 +0100

    Update AssemblyInfo.cs

--- ParaSyndra/ParaSyndra/Properties/AssemblyInfo.cs
+++ ParaSyndra/ParaSyndra/Properties/AssemblyInfo.cs
@@ -24,5 +24,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can use the default the Revision and 
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion ("1.0.0.6")]
+[assembly: AssemblyVersion ("1.0.0.7")]
 [assembly: Guid ("5168ef5b-e836-406b-af64-c7ddc14d7c5e")]
