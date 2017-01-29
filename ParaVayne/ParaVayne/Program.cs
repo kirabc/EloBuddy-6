b4e6b5635d7c87441c48b996f3cd53c3135bcd8d
mar173 <kiker173@wp.pl>
2017-01-29 18:59:12 +0100

    1.0.0.1

--- ParaVayne/ParaVayne/Program.cs
+++ ParaVayne/ParaVayne/Program.cs
@@ -10,9 +10,11 @@ namespace ParaVayne
 {
 	class Program
 	{
+		static AttackableUnit lasthit;
+		
 		static Menu menu;
         
-		static float lastaa, lastmove, aacastdelay, aadelay;
+		static float lastaa, lastmove, aacastdelay, aadelay, lastminion;
         
 		public static void Main(string[] args)
 		{
@@ -25,6 +27,7 @@ namespace ParaVayne
 				return;
 			menu = MainMenu.AddMenu("ParaVayne", "paravayne");
 			menu.Add("combo", new KeyBind("Combo", false, KeyBind.BindTypes.HoldActive, ' '));
+			menu.Add("lasthit", new KeyBind("LastHit", false, KeyBind.BindTypes.HoldActive, 'X'));
 			Game.OnUpdate += Game_OnTick;
 			Obj_AI_Base.OnBasicAttack += Obj_AI_Base_OnBasicAttack;
 			Obj_AI_Base.OnBuffGain += Obj_AI_Base_OnBuffGain;
@@ -36,7 +39,13 @@ namespace ParaVayne
 			{
 				Orbwalker.DisableMovement = true;
 				Orbwalker.DisableAttacking = true;
-				Orb();
+				Combo();
+			}
+			else if (menu["lasthit"].Cast<KeyBind>().CurrentValue)
+			{
+				Orbwalker.DisableMovement = true;
+				Orbwalker.DisableAttacking = true;
+				LastHit();
 			}
 			else
 			{
@@ -45,7 +54,42 @@ namespace ParaVayne
 			}
 		}
 		
-		static void Orb()
+		static void LastHit()
+		{
+			if (Game.Time < lastminion + 0.5f && Game.Time + 0.2f > lastaa + aadelay)
+			{
+				Player.IssueOrder(GameObjectOrder.AttackUnit, lasthit);
+				return;
+			}
+			
+			if (Game.Time > lastaa + aacastdelay + 0.05f && Game.Time > lastmove + 0.2f)
+			{
+				Player.IssueOrder(GameObjectOrder.MoveTo, Game.CursorPos);
+				lastmove = Game.Time;
+				lastminion = 0;
+			}
+			
+			if (Game.Time + 0.2f < lastaa + aadelay)
+				return;
+			foreach (var Minion in EntityManager.MinionsAndMonsters.Minions
+						.Where(m => m.IsValidTarget(Player.Instance.AttackRange + Player.Instance.BoundingRadius + m.BoundingRadius, true))
+						.OrderBy(m => m.CharData.BaseSkinName.Contains("Siege"))
+						.ThenBy(m => m.CharData.BaseSkinName.Contains("Super"))
+						.ThenBy(m => m.Health)
+						.ThenByDescending(m => m.MaxHealth))
+			{
+				var healthPred = Prediction.Health.GetPrediction(Minion, (int)(Player.Instance.AttackCastDelay * 1000) + 1000 * (int)(Math.Max(0, Player.Instance.Distance(Minion) - Minion.BoundingRadius) / (int)Player.Instance.BasicAttack.MissileSpeed));
+				if (healthPred <= Player.Instance.GetAutoAttackDamage(Minion))
+				{
+					lasthit = Minion;
+					lastminion = Game.Time;
+					Player.IssueOrder(GameObjectOrder.AttackUnit, lasthit);
+					return;
+				}
+			}
+		}
+		
+		static void Combo()
 		{
 			if (Player.CanUseSpell(SpellSlot.Q) == SpellState.Ready && Game.Time > lastaa + aacastdelay + 0.025f && Game.Time < lastaa + (aadelay * 0.75f))
 			{
