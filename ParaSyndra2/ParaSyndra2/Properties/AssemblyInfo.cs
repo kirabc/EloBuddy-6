3657661c7f8cd8064d467a1bb79d3a09e360083b
mar173 <kiker173@wp.pl>
2017-01-30 21:11:50 +0100

    Create AssemblyInfo.cs

---
+++ ParaSyndra2/ParaSyndra2/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTitle("ParaSyndra2")]
+[assembly: AssemblyDescription("Addon for EloBuddy")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("mar173")]
+[assembly: AssemblyProduct("ParaSyndra2")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("114eb14f-86c5-46c2-b94c-c29ce346d79c")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
