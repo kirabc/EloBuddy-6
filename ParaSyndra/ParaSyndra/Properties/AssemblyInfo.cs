c3f11423e98d8ff51438fb044ee745572bb053e5
mar173 <kiker173@wp.pl>
2017-01-31 16:30:31 +0100

    Update AssemblyInfo.cs

--- ParaSyndra/ParaSyndra/Properties/AssemblyInfo.cs
+++ ParaSyndra/ParaSyndra/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("114eb14f-86c5-46c2-b94c-c29ce346d79c")]
-[assembly: AssemblyVersion("1.0.0.8")]
-[assembly: AssemblyFileVersion("1.0.0.8")]
+[assembly: AssemblyVersion("1.0.0.9")]
+[assembly: AssemblyFileVersion("1.0.0.9")]
