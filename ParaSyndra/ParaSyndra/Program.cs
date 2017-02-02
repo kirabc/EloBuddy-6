7fc602d4b92849c9af5c34d5faf6ce6af66d9f76
mar173 <kiker173@wp.pl>
2017-02-02 15:04:32 +0100

    Update Program.cs

--- ParaSyndra/ParaSyndra/Program.cs
+++ ParaSyndra/ParaSyndra/Program.cs
@@ -265,7 +265,7 @@ namespace ParaSyndra
 				var enemy = TargetSelector.GetTarget(1050, DamageType.Magical);
 				if (!enemy.IsValidTarget())
 					return;
-				CastSpell(SpellSlot.W, enemy, 950, 1450, 0f);
+				CastSpell(SpellSlot.W, enemy, 950, 2000, 0f);
 				return;
 			}
 			
